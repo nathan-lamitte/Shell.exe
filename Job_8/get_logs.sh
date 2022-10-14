e7b4dc7edb523769000109c75a9082bd1e34c020
frp <frp@debianathan>
2022-10-14 11:33:26 +0200

    Job_8 push

--- Job_8/get_logs.sh
+++ Job_8/get_logs.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 
+jour=$(date +%d-%m-%Y-%H:%M)
+
+fichier=number_connection-$jour.log
+
+sudo grep -w "opened for user deb" -c /var/log/auth.log >> ./$fichier
+tar -cvf ./$fichier.tar.gz $fichier
+
+mv $fichier.tar.gz ./Backup
+
+rm $fichier
